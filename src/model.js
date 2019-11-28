13457ed2f7aac215508c6ce6a56127a19d33a452
germanherrou <german.herrou@gmail.com>
2019-11-28 20:31:35 -0300

    Mostar arreglo. Click en celda cambia color

---
+++ src/model.js
@@ -0,0 +1,9 @@
+var array = [1, 1, 0, 1, 1];
+
+function getArray(){
+    return array;
+}
+
+function update(index, value) {
+    array[index] = value;
+}
---
+++ src/view.js
@@ -0,0 +1,61 @@
+var stage = new Konva.Stage({
+    container: 'container',   // id of container <div>
+    width: 500,
+    height: 500
+});
+
+// then create layer
+var layer = new Konva.Layer();
+
+side = 50;
+var array = getArray();
+var squares = [];
+var color = 'white'
+for (let index = 0; index < array.length; index++) {
+    if (array[index] == 1)
+        color = 'gray'
+    else
+        color = 'white'
+
+
+    var square = new Konva.Rect({
+        id: index,
+        x: index * side,
+        y: 20,
+        width: side,
+        height: side,
+        fill: color,
+        stroke: 'black',
+        strokeWidth: 4
+    });
+
+    square.on('click', function (info) {
+        value = 0;
+        if (this.fill() == 'gray')
+        {
+            this.fill('white');
+        }
+        else
+        {
+            this.fill('gray');
+            value = 1;
+        }
+        layer.draw();
+        update(this.index, value);
+    })
+
+    squares.push(square);
+
+}
+
+
+// add the shape to the layer
+squares.forEach(square => {
+    layer.add(square);
+});
+
+// add the layer to the stage
+stage.add(layer);
+
+// draw the image
+layer.draw();
