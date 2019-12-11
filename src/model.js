68bf5e65d350f09cf2007bf2338dd25a554b6f6e
germanherrou <german.herrou@gmail.com>
2019-12-11 15:29:21 -0300

    Transición entre estados en funcion de regla

--- src/model.js
+++ src/model.js
@@ -1,6 +1,7 @@
 var array = [1, 1, 0, 1, 1];
+var rule = "00110010";
 
-function getArray(){
+function getArray() {
     return array;
 }
 
@@ -12,21 +13,24 @@ function transition() {
     var newArray = [];
     var value = 0;
     for (let index = 0; index < array.length; index++) {
-        var blacks = 0;
         if (index == 0)
-            blacks = array[index] + array[(array.length - 1)] + array[index + 1];
+            value = evolve(array[(array.length - 1)], array[index], array[index + 1]);
         else if (index == (array.length - 1))
-            blacks = array[index] + array[index - 1] + array[0];
+            value = evolve(array[index - 1], array[index], array[0]);
         else
-            blacks = array[index] + array[index - 1] + array[index + 1];
-        if (blacks == 1)
-            value = 1;
-        else
-            value = 0;
+            value = evolve(array[index - 1], array[index], array[index + 1]);
 
         newArray.push(value);
 
     }
     array = newArray;
 
+}
+
+function evolve(previous, current, next) {
+    var configurationBinary = previous * 100 + current * 10 + next;
+    var configurationDecimal = parseInt(configurationBinary, 2);
+    var ruleValueChar = rule.charAt(7 - configurationDecimal);
+    var ruleValue = parseInt(ruleValueChar, 10);
+    return ruleValue;
 }
