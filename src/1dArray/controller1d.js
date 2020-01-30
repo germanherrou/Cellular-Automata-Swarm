71e650fb407881795030a77618b2762dabbb34a2
germanherrou <german.herrou@gmail.com>
2020-01-30 19:57:51 -0300

    Cartel simulación en curso

--- src/1dArray/controller1d.js
+++ src/1dArray/controller1d.js
@@ -6,6 +6,9 @@ function start() {
         disableBars(true);
         running = true;
         timer = setInterval(next, 1000);
+        var texto = document.getElementById("simulacion");
+        texto.innerHTML = "Simulación en curso";
+        texto.style.color = "green";
     }
 
 }
@@ -14,6 +17,9 @@ function stop() {
     clearInterval(timer);
     running = false;
     disableBars(false);
+    var texto = document.getElementById("simulacion");
+    texto.innerHTML = "Simulación detenida";
+    texto.style.color = "red";
 }
 
 function isRunning() {
