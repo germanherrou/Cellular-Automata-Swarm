a84d37b62fde62f901af430476e9bb04e1077ed0
germanherrou <german.herrou@gmail.com>
2020-01-31 02:30:39 -0300

    Modelo enjambre básico

---
+++ swarm/modelSwarm.js
@@ -0,0 +1,11 @@
+var size = 4;
+var grid = [];
+for (let index = 0; index < size; index++) {
+    var row = [];
+    for (let index = 0; index < size; index++) {
+        var number = Math.floor(Math.random() * 4);
+        var cell = new SwarmCell(number, 1, 2, 3);
+        row.push(cell);
+    }
+    grid.push(row);
+}
---
+++ swarm/swarmCell.js
@@ -0,0 +1,8 @@
+class SwarmCell {
+    constructor(tribe, food, war, culture){
+        this.tribe = tribe;
+        this.food = food;
+        this.war = war;
+        this.culture = culture;
+    }
+}
