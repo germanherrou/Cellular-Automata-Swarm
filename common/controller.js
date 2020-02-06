bc19e03e1de1a90efbdef4d3952ccb4890603af3
germanherrou <german.herrou@gmail.com>
2020-02-06 17:43:26 -0300

    Refactorizar temporizador

--- common/controller.js
+++ common/controller.js
@@ -1,31 +1,3 @@
-var timer;
-var running = false;
-
-function start() {
-    if (!running) {
-        disableControls(true);
-        running = true;
-        timer = setInterval(next, 1000);
-        var texto = document.getElementById("simulacion");
-        texto.innerHTML = "Simulación en curso";
-        texto.style.color = "green";
-    }
-
-}
-
-function stop() {
-    clearInterval(timer);
-    running = false;
-    disableControls(false);
-    var texto = document.getElementById("simulacion");
-    texto.innerHTML = "Simulación detenida";
-    texto.style.color = "red";
-}
-
-function isRunning() {
-    return running;
-}
-
 function next() {
     transition();
     updateImage(getGrid());
--- common/view.js
+++ common/view.js
@@ -1,3 +1,31 @@
+var timer;
+var running = false;
+
+function start() {
+    if (!running) {
+        disableControls(true);
+        running = true;
+        timer = setInterval(next, 1000);
+        var texto = document.getElementById("simulacion");
+        texto.innerHTML = "Simulación en curso";
+        texto.style.color = "green";
+    }
+
+}
+
+function stop() {
+    clearInterval(timer);
+    running = false;
+    disableControls(false);
+    var texto = document.getElementById("simulacion");
+    texto.innerHTML = "Simulación detenida";
+    texto.style.color = "red";
+}
+
+function isRunning() {
+    return running;
+}
+
 function disableControls(state) {
     var elements = document.getElementsByClassName("settings-configuration");
     for (let element of elements) {
