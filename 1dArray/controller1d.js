d33ddd92d429b5b8d7ce14610de7ff3b887f58b5
germanherrou <german.herrou@gmail.com>
2020-02-06 16:50:30 -0300

    Cambiar regla 1d solo si está dentro del rango

--- 1dArray/controller1d.js
+++ 1dArray/controller1d.js
@@ -1,12 +1,13 @@
 
 function changeRule(value) {
-    if (value >= 0 && value < 256)
+    if (value >= 0 && value < 256) {
         valueString = parseInt(value, 10).toString(2);
-    valueString = "00000000".substr(valueString.length) + valueString;
-    var texto = document.getElementById("ruleRange");
-    texto.innerHTML = value;
-    updateRule(valueString);
-    updateResults(valueString);
+        valueString = "00000000".substr(valueString.length) + valueString;
+        var texto = document.getElementById("ruleRange");
+        texto.innerHTML = value;
+        updateRule(valueString);
+        updateResults(valueString);
+    }
 }
 
 function resize(newSize) {
