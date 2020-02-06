af66dd4f8e836d5e1b7f0423fcfc3ebc577eac9c
germanherrou <german.herrou@gmail.com>
2020-02-06 18:07:33 -0300

    Refactorización gráficos 2d

---
+++ 2dGrid/konva2d.js
@@ -0,0 +1,74 @@
+var maxWidth = 780;
+var stage = new Konva.Stage({
+    container: 'container2d', // id of container <div>
+    width: maxWidth,
+    height: 780,
+});
+var layer = new Konva.Layer();
+var grid = getGrid();
+var squares = [];
+var defaultSide = 50;
+var side = defaultSide;
+
+function updateImage(grid) {
+    squares.forEach(square => {
+        square.remove();
+    });
+    squares = [];
+
+    var color = 'white';
+
+    for (let rowIndex = 0; rowIndex < grid.length; rowIndex++) {
+        const row = grid[rowIndex];
+        for (let columnIndex = 0; columnIndex < row.length; columnIndex++) {
+            const element = grid[rowIndex][columnIndex];
+
+            if (element)
+                color = 'gray'
+            else
+                color = 'white'
+
+            var square = new Konva.Rect({
+                id: (rowIndex * 10) + columnIndex,
+                x: columnIndex * side,
+                y: offsetHeight + rowIndex * side,
+                width: side,
+                height: side,
+                fill: color,
+                stroke: 'black',
+                strokeWidth: 4
+            });
+
+            square.on('click tap', function (info) {
+                if (!isRunning()) {
+                    value = 0;
+                    if (this.fill() == 'gray') {
+                        this.fill('white');
+                    } else {
+                        this.fill('gray');
+                        value = 1;
+                    }
+                    layer.draw();
+                    var row = (this.attrs.y - 20) / side;
+                    var column = this.attrs.x / side;
+                    update(row, column, value);
+                }
+            })
+
+            squares.push(square);
+
+        }
+
+    }
+
+    // add the shape to the layer
+    squares.forEach(square => {
+        layer.add(square);
+    });
+
+    // add the layer to the stage
+    stage.add(layer);
+
+    // draw the image
+    layer.draw();
+}
--- 2dGrid/view2d.js
+++ 2dGrid/view2d.js
@@ -1,74 +1,5 @@
-var layer = new Konva.Layer();
-var defaultSide = 50;
-var side = defaultSide;
-var grid = getGrid();
-var squares = [];
 var offsetWidth = 45;
 var offsetHeight = 20;
-var maxWidth = 780;
-
-function updateImage(grid) {
-    squares.forEach(square => {
-        square.remove();
-    });
-    squares = [];
-
-    var color = 'white';
-
-    for (let rowIndex = 0; rowIndex < grid.length; rowIndex++) {
-        const row = grid[rowIndex];
-        for (let columnIndex = 0; columnIndex < row.length; columnIndex++) {
-            const element = grid[rowIndex][columnIndex];
-
-            if (element)
-                color = 'gray'
-            else
-                color = 'white'
-
-            var square = new Konva.Rect({
-                id: (rowIndex * 10) + columnIndex,
-                x: columnIndex * side,
-                y: offsetHeight + rowIndex * side,
-                width: side,
-                height: side,
-                fill: color,
-                stroke: 'black',
-                strokeWidth: 4
-            });
-
-            square.on('click tap', function (info) {
-                if (!isRunning()) {
-                    value = 0;
-                    if (this.fill() == 'gray') {
-                        this.fill('white');
-                    } else {
-                        this.fill('gray');
-                        value = 1;
-                    }
-                    layer.draw();
-                    var row = (this.attrs.y - 20) / side;
-                    var column = this.attrs.x / side;
-                    update(row, column, value);
-                }
-            })
-
-            squares.push(square);
-
-        }
-
-    }
-
-    // add the shape to the layer
-    squares.forEach(square => {
-        layer.add(square);
-    });
-
-    // add the layer to the stage
-    stage.add(layer);
-
-    // draw the image
-    layer.draw();
-}
 
 function fitStageInConainer() {
     var container = document.getElementById("stage-container");
@@ -91,13 +22,6 @@ function fitStageInConainer() {
 
     updateImage(grid);
 }
-
-
-var stage = new Konva.Stage({
-    container: 'container2d', // id of container <div>
-    width: maxWidth,
-    height: 780,
-});
 
 window.addEventListener('resize', fitStageInConainer);
 fitStageInConainer();
