371b2648ccfdf1f5e3f45020a98a19161122ceb4
germanherrou <german.herrou@gmail.com>
2020-02-07 19:17:10 -0300

    Mantener botones 5-9 deshabilitados en Von Neumann

--- 2dGrid/controller2d.js
+++ 2dGrid/controller2d.js
@@ -1,16 +1,6 @@
 function changeNeighborhood() {
     toogleNeighborhood();
-    var elements = document.getElementsByClassName("mooreNeighborhood");
-    for (let element of elements) {
-        if (isMooreNeighborhood()) {
-            element.disabled = false;
-        }
-        else {
-            element.checked = false;
-            element.disabled = true;
-        }
-
-    }
+    setNeighborhoodButtons();
 }
 
 function changeRows(total) {
--- 2dGrid/view2d.js
+++ 2dGrid/view2d.js
@@ -1,6 +1,24 @@
 var offsetWidth = 45;
 var offsetHeight = 20;
 
+function setNeighborhoodButtons() {
+    var elements = document.getElementsByClassName("mooreNeighborhood");
+    for (let element of elements) {
+        if (isMooreNeighborhood()) {
+            element.disabled = false;
+        }
+        else {
+            element.checked = false;
+            element.disabled = true;
+        }
+    }
+}
+
+function stop2d() {
+    stop();
+    setNeighborhoodButtons();
+}
+
 function fitStageInConainer() {
     var container = document.getElementById("stage-container");
     var containerWidth = container.offsetWidth - offsetWidth;
